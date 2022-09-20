a3948df89ae051bbf1d06ec92afd714683926efd
Sridhar Madala <smadala2@binghamton.edu>
2022-09-20 00:07:07 -0400

    main file

---
+++ desig-inits.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#sets dir to directory containing this script
+dir=/home/smadala2/projects/cs571/projects/prj1/prj1-sol/
+
+#use $dir/ as prefix to run any programs in this dir
+#so that this script can be run from any directory.
+
+python3 $dir/project1.py
